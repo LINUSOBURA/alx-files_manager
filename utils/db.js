28b25e6c4ea0341b849dc0a25a729f9b1408b9bb
LINUSOBURA <linusobura@gmail.com>
2024-09-02 21:08:15 +0300

    task 1

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+
+    this.url = `mongodb://${this.host}:${this.port}/`;
+    this.dbconnected = false;
+    this.db = null;
+    MongoClient.connect(this.url, { useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.db = client.db(this.database);
+        this.dbconnected = true;
+      }
+    });
+  }
+
+  isAlive() {
+    return this.dbconnected;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
