f5c568e2eef650af06bd1a6bba24fd3fa63fe0ca
LINUSOBURA <linusobura@gmail.com>
2024-09-05 00:20:36 +0300

    task 8

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const mime = require('mime-types');
 const { ObjectId } = require('mongodb');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
@@ -217,6 +218,41 @@ class FilesController {
       });
 
       return res.status(200).json(updatedFile);
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getFile(req, res) {
+    const fileId = req.params.id;
+
+    try {
+      const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId) });
+
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      if (file.type === 'folder') {
+        return res.status(400).json({ error: "A folder doesn't have content" });
+      }
+
+      const token = req.headers['x-token'];
+      const userId = token ? await redisClient.get(`auth_${token}`) : null;
+
+      if (!file.isPublic && (!userId || userId !== file.userId.toString())) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      if (!fs.existsSync(file.localPath)) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      const mimeType = mime.lookup(file.name);
+      res.setHeader('Content-Type', mimeType);
+
+      const fileStream = fs.createReadStream(file.localPath);
+      fileStream.pipe(res);
     } catch (err) {
       return res.status(500).json({ error: 'Internal server error' });
     }
